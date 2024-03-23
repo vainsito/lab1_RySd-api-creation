4dce2dfce5d6c88e73d4a8013955241da7932c93
Alexis <Alexis.ortiz@mi.unc.edu.ar>
2024-03-23 15:59:55 -0300

    Test funciones parte 2

--- lab1-kickstarter/test.py
+++ lab1-kickstarter/test.py
@@ -55,34 +55,46 @@ if response.status_code == 200:
     print("Película eliminada correctamente.")
 else:
     print("Error al eliminar la película.")
+print()
     
 # Filtrar películas por género
-genero = 'Drama'  # Género por el que filtrar
+genero = 'Acción'  # Género por el que filtrar
 response = requests.get(f'http://localhost:5000/peliculas/genero/{genero}')
-if response.status_code == 200:
+if response.status_code == 201:
     peliculas_filtradas = response.json()
     print(f"Películas de género '{genero}' son:")
-    for pelicula in peliculas_filtradas:
-        print(f"ID: {pelicula['id']}, Título: {pelicula['titulo']}, Género: {pelicula['genero']}")
+    print(peliculas_filtradas)
 else:
     print("Error al obtener las películas por género.")
+print()
     
 # Filtrar peliculas por palabra clave
-palabra = 'El'
+palabra = 'In'
 response = requests.get(f'http://localhost:5000/peliculas/titulo/{palabra}')
-if response.status_code == 200:
+if response.status_code == 201:
     peliculas_filtradas = response.json()
     print(f"Películas con la palabra clave '{palabra}' son:")
-    for pelicula in peliculas_filtradas:
-        print(f"ID: {pelicula['id']}, Título: {pelicula['titulo']}, Género: {pelicula['genero']}")
+    print(peliculas_filtradas)
 else:
     print("Error al obtener las películas por palabra clave.")
+print()
 
 # Pelicula random
 response = requests.get('http://localhost:5000/peliculas/random')
-if response.status_code == 200:
+if response.status_code == 201:
     pelicula_random = response.json()
     print("Película aleatoria:")
-    print(f"ID: {pelicula_random['id']}, Título: {pelicula_random['titulo']}, Género: {pelicula_random['genero']}")
+    print(pelicula_random)
 else:
     print("Error al obtener la película aleatoria.")
+print()
+
+genero = 'Acción'  # Género por el que filtrar
+response = requests.get(f'http://localhost:5000/peliculas/random/{genero}')
+if response.status_code == 201:
+    pelicula_aleatoria_genero = response.json()
+    print(f"Películas de género '{genero}' son:")
+    print( pelicula_aleatoria_genero)
+else:
+    print("Error al obtener una pelicula aleatoria del genero solicitado")
+print()
